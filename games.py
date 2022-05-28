e1b9409ec52de1b58d73041b812f922f5938b3f4
Kelvin <0.darns.speck@icloud.com>
2022-05-28 12:30:53 +0800

    Update games.py

--- games.py
+++ games.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import asyncio
 import random
 import json, os
+import asyncio
 
 directory = os.getcwd()
 
@@ -17,7 +18,15 @@ class games(commands.Cog):
         player = ctx.author
         answer = random.choice(wordList).lower()
         guessCount = 0
-        await ctx.author.send("This feature is currently under development. Stay tuned!")
+        await ctx.send("The word has been generated. Start guessing by typing a 5-letter word! Note that if no input is detected for 60 seconds, the bot timeouts.")
+        try:
+            for guessCount in range(5):
+                message = await self.bot.wait_for("message", check = lambda message: message.author == ctx.author, timeout= 60.0)
+                
+
+        except asyncio.TimeoutError():
+            await ctx.send(f"As no input is detected, the wordle has ended. The answer is: {answer.lower()}")
+
 
 def setup(bot):
     bot.add_cog(games(bot))
