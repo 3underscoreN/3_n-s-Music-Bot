9ede071b7ac7c20b1c8d78653432d3d2b07040d0
Kelvin <0.darns.speck@icloud.com>
2022-10-19 22:22:49 +0800

    Updated file structure for storing command info and commands

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import disnake
-from disnake.ext import commands
+from disnake.ext import commands as disnakeCommands
 import music
 import games
 import json
@@ -7,6 +7,7 @@ from difflib import get_close_matches
 import os
 import random
 import logging
+import itertools
 
 errorID = 0
 logging.basicConfig(
@@ -22,26 +23,18 @@ intents.message_content = True
 
 cogs = [music, games]
 myid = int(os.getenv('OWNER'))
-bot = commands.Bot(
+bot = disnakeCommands.Bot(
     command_prefix='k!', 
     intents = intents,
     sync_commmands_debug = True
 )
+
+# help command related
 bot.remove_command('help')
-
-helpText = json.loads(open("./commands/helpText.json").read())
-COMMANDS_MUSIC = json.loads(open("./commands/commands_music.json").read())
-COMMANDS_GAMES = json.loads(open("./commands/commands_games.json").read())
-
-COMMANDS = [*COMMANDS_MUSIC, *COMMANDS_GAMES, "help", "shutdown", "about"]
-comString_music = ''
-comString_games = ''
-comString_other = "help \n ping \n shutdown \n about"
-
-for i in COMMANDS_MUSIC:
-    comString_music += (i + '\n')
-for i in COMMANDS_GAMES:
-    comString_games += (i + '\n')
+COMMANDS = json.loads(open("./commands/commands.json").read())
+COMMANDS_INFO = json.loads(open("./commands/commandsInfo.json").read())
+COMMANDS_INFO_HEADING = ["format", "aliases", "description", "examples"]
+commands_unordered = list(itertools.chain.from_iterable(COMMANDS.values()))
 
 for i in range(len(cogs)):
     cogs[i].setup(bot)
@@ -60,7 +53,7 @@ async def shutdown(ctx):
         await ctx.send(embed = embed)
         await bot.close()
     else:
-        raise commands.NotOwner()
+        raise disnakeCommands.NotOwner()
 
 #ping
 @bot.command()
@@ -76,27 +69,31 @@ async def raiseException(ctx):
     if ctx.message.author.id == myid:
         raise Exception("This exception is raised for debugging purpose. No action is required.")
     else:
-        raise commands.NotOwner()
+        raise disnakeCommands.NotOwner()
 
 @bot.command()
 async def help(ctx, command = None):
     if command == None or command == "None":
         embed = disnake.Embed(title="**Help Panel**", description="Here is a list of commands the bot has!\n\nUse `k!help [command]` to get detailed info about a specific command.", color = 0x11f1f5)
-        embed.add_field(name="Music", value = comString_music, inline=True)
-        embed.add_field(name="Games", value = comString_games, inline = True)
-        embed.add_field(name="Others", value = comString_other, inline=True)
+        for i in COMMANDS.keys():
+            tString = "" # temp string only, can be discarded
+            for j in COMMANDS[i]:
+                tString += f"{j}\n"
+            embed.add_field(name = i.capitalize(), value = tString[:-1:], inline = True)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed = embed)
     else:
         try:
             embed = disnake.Embed(title="**Help Panel**", description = "{0}".format(command), color=0x11f1f5)
-            commandDetails = helpText[command]
-            commandHeading = ["Attributes", "Aliases", "Description", "Examples"]
-            for i in range(4):
-                embed.add_field(name = f"{commandHeading[i]}", value = f"{commandDetails[i]}", inline = False)
+            tDetails = COMMANDS_INFO[command.lower()] # temp, can be discarded
+            for i in COMMANDS_INFO_HEADING:
+                if type(tDetails[i]) != type([]):
+                    embed.add_field(name = i.capitalize(), value = f"`{tDetails[i]}`" if i == "format" else f"{tDetails[i]}", inline = False)
+                else:
+                    embed.add_field(name = i.capitalize(), value = ", ".join(tDetails[i]) if i == "aliases" else "`" + "`\n`".join(tDetails[i]) + "`", inline = False)
             await ctx.send(embed = embed)
-        except:
-            potentialCommand = get_close_matches(command, COMMANDS)
+        except KeyError: # command not found
+            potentialCommand = get_close_matches(command, commands_unordered)
             potential = ""
             for i in potentialCommand:
                 potential += f"{i}, "
@@ -125,49 +122,49 @@ async def about(ctx):
 @bot.event
 async def on_command_error(ctx, error):
     errorID = ''
-    if isinstance(error, commands.CommandNotFound):
+    if isinstance(error, disnakeCommands.CommandNotFound):
         embed=disnake.Embed(title="Error: Command not found", color=0xff0000)
-        embed.add_field(name="The command you entered does not seem to be valid.", value="Please double-check your entry. If you don't know what you are doing, you can use `k!help` for a list of commands availabe.\n\nYou can also use `k!help [command]` to find details about a specific command.\nIf you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
+        embed.add_field(name="The command you entered does not seem to be valid.", value="Please double-check your entry. If you don't know what you are doing, you can use `k!help` for a list of disnakeCommands availabe.\n\nYou can also use `k!help [command]` to find details about a specific command.\nIf you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed=embed)
         return
 
-    if isinstance(error, commands.UserInputError):
+    if isinstance(error, disnakeCommands.UserInputError):
         embed = disnake.Embed(title = "Error: User Input Error", color = 0xff0000)
         embed.add_field(name = "We have detected an error in your input.", value = "Please check your parameters to see if they are in the right formation or are entered correctly.\nIf you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).")
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed = embed)
         return
 
-    if isinstance(error, commands.MissingRequiredArgument):
+    if isinstance(error, disnakeCommands.MissingRequiredArgument):
         embed=disnake.Embed(title="Error: Missing arguments", color=0xff0000)
         embed.add_field(name="Some required arguments in your entry seems to be missing.", value="Please double-check your entry. You can use `k!help [command]` to find all the required arguments (usually bracketed by `<>`) in the command.\nIf you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed=embed)
         return
 
-    if isinstance(error, commands.TooManyArguments):
+    if isinstance(error, disnakeCommands.TooManyArguments):
         embed=disnake.Embed(title="Error: Too many arguments", color=0xff0000)
         embed.add_field(name="You have entered too many arguments in your command.", value="Please double-check your entry. You can use `k!help [command]` to find the arguments that are redundant in your command.\nIf you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed=embed)
         return
 
-    if isinstance(error, commands.NotOwner):
+    if isinstance(error, disnakeCommands.NotOwner):
         embed=disnake.Embed(title="Error: Access Denied", color=0xff0000)
         embed.add_field(name="It seems like only the owner can execute the command.", value="Please double-check your entry. If you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed=embed)
         return
 
-    if isinstance(error, commands.BotMissingPermissions):
+    if isinstance(error, disnakeCommands.BotMissingPermissions):
         embed=disnake.Embed(title="Error: Bot missing permission", color=0xff0000)
         embed.add_field(name="It seems like the bot doesn't have permission to do so", value="Please politely ask moderators to fix this issue. If you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
         await ctx.send(embed=embed)
         return
 
-    if isinstance(error, commands.NoPrivateMessage):
+    if isinstance(error, disnakeCommands.NoPrivateMessage):
         embed=disnake.Embed(title="Error: No Direct Message", color=0xff0000)
         embed.add_field(name="This command can't run in direct message.", value="Please run the command in a server text channel. If you believe this is a bug, please open an issue on [Github project page](https://github.com/3underscoreN/3_n-s-Music-Bot).", inline=False)
         embed.set_footer(text="Bot made by 3_n#7069")
@@ -177,7 +174,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, music.ExceptionResolved):
         return
 
-    if isinstance(error, commands.CommandInvokeError):
+    if isinstance(error, disnakeCommands.CommandInvokeError):
         for _ in range(6):
             errorID += str(random.choice(range(10)))
         embed=disnake.Embed(title="Error: Unexpected error", color=0xff0000)
